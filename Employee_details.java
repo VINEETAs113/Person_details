96616a8807f7fc11e9f56c48815a194a9ed35d9c
VINEETAs113 <nanu.vineeta@gmail.com>
2024-03-18 02:22:48 -0700

    Updated code with emp_id and emp_salary

---
+++ Employee_details.java
@@ -0,0 +1,12 @@
+public class Employee_details{
+
+     
+    static float emp_id (float x){
+        return x;
+    }
+    
+    static float emp_salary (float y){
+        return y;
+    }
+    
+}
--- Person.java
+++ Person.java
@@ -1,7 +1,10 @@
 import java.time.*;
 
-public class Person {
+public class Person extends Employee_details {
 	public static void main(String[] args) {
+
+		float emp_id = 1234;
+		float emp_salary = 50000;
 
 		LocalDate pdt = LocalDate.of(2004, 03, 23);
 		LocalDate tdt = LocalDate.now();
@@ -9,6 +12,8 @@ public class Person {
 		System.out.println("Name: Vineeta");
 		System.out.println("Date of Birth : " + pdt);
 		System.out.println("Age : " + diff.getYears());
+		System.out.println("Emp_id : " + emp_id(emp_id));
+		System.out.println("Emp_salary : " + emp_salary(emp_salary));
 
 	}
 }
